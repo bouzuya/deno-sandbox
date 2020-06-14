8b8f61686c7836804b174462de4acb0ce2759057
bouzuya <m@bouzuya.net>
2020-06-14 12:55:54 +0900

    add accepted-count

---
+++ accepted-count/README.md
@@ -0,0 +1,19 @@
+# accepted-count
+
+## Installation
+
+```bash
+$ cache_path=/Users/bouzuya/accepted-count-cache.json
+$ deno install --allow-net=kenkoooo.com --allow-read="${cache_path}" --allow-write="${cache_path}" --unstable main.ts --cache-path "${cache_path}"
+# ...
+Compile file:///Users/bouzuya/ghq/github.com/bouzuya/deno-sandbox/accepted-count/main.ts
+✅ Successfully installed accepted-count
+/Users/bouzuya/.deno/bin/accepted-count
+```
+
+## Usage
+
+```bash
+$ accepted-count bouzuya
+517
+```
---
+++ accepted-count/mod.ts
@@ -0,0 +1,88 @@
+import * as flags from "https://deno.land/std@v0.57.0/flags/mod.ts";
+import * as fs from "https://deno.land/std@v0.57.0/fs/mod.ts";
+
+type AcceptedCount = Array<{ user_id: string; problem_count: number }>;
+
+type Response = {
+  body: AcceptedCount;
+  etag: string;
+};
+
+export async function loadCache(cachePath: string): Promise<Response | null> {
+  if (!await fs.exists(cachePath)) return null;
+  try {
+    const json = await fs.readJson(cachePath);
+    // TODO: validate json
+    if (typeof json !== "object" || json === null) return null;
+    const o = json as { [k: string]: unknown };
+    const body = o.body;
+    if (!Array.isArray(body)) return null;
+    const etag = o.etag;
+    if (typeof etag !== "string") return null;
+    return { body, etag };
+  } catch {
+    return null;
+  }
+}
+
+export async function saveCache(
+  cachePath: string,
+  cache: Response,
+): Promise<void> {
+  return fs.writeJson(cachePath, cache);
+}
+
+async function loadAllAcceptedCount(
+  options: { cachePath: string },
+): Promise<AcceptedCount> {
+  const { cachePath } = options;
+  const cache = await loadCache(cachePath);
+  const response = await fetch(
+    "https://kenkoooo.com/atcoder/resources/ac.json",
+    {
+      headers: {
+        "Accept": "application/json",
+        ...(cache !== null ? { "If-None-Match": cache.etag } : {}),
+      },
+    },
+  );
+  if (response.status === 200) {
+    const body = await response.json();
+    const etag = response.headers.get("etag");
+    if (etag !== null) await saveCache(cachePath, { body, etag });
+    return body;
+  } else if (response.status === 304 && cache !== null) {
+    return cache.body;
+  } else {
+    throw new Error(
+      `invalid status: ${response.status} or status: 304 && cache === null`,
+    );
+  }
+}
+
+export function parseArgs(
+  args: string[],
+): { cachePath: string; userName: string } {
+  const parsed = flags.parse(args, {
+    string: ["cache-path"],
+  });
+  const userName = parsed["_"][0];
+  if (typeof userName !== "string") {
+    throw new Error("userName is not string");
+  }
+  const cachePath = parsed["cache-path"];
+  if (typeof cachePath !== "string") {
+    throw new Error("cachePath is not string");
+  }
+  return { cachePath, userName };
+}
+
+export async function main(): Promise<void> {
+  const { cachePath, userName } = parseArgs(Deno.args);
+  const acceptedCount = await loadAllAcceptedCount({ cachePath });
+  const foundUser = acceptedCount.find(({ user_id }) => user_id === userName);
+  if (typeof foundUser === "undefined") {
+    throw new Error(`user "${userName}" not found`);
+  }
+  console.log(foundUser.problem_count);
+}
